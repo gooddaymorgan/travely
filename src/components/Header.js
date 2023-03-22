f51946dfd28c645ff99a180108a60f4436269ffd
Morgan <morgan.em.day@gmail.com>
2023-03-22 14:43:25 -0600

    Header Updates
    -images not working

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = (props) => (
+    <header>
+        <a href="#">Logo</a>
+
+        <div>
+            <a href="#">About</a>
+            <a href="#">Places</a>
+            <a href="#">Booking</a>
+            <a href="#">Profile</a>
+        </div>
+    </header>
+);
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import hero from '../assests/hero.jpg';
+
+const Hero = (props) => (
+      <div>
+        <img src={hero} alt="#" width="100%" height="auto"/>
+          <h1>
+            Broaden Your
+            Horizons
+          </h1>
+      </div>
+
+);
+
+export default Hero;
